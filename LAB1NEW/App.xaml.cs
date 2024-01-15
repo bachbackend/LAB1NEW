052ba7e0a0233fa17f21b6250610feeb53623b27
bachbackend <lebach09122002@gmail.com>
2024-01-15 15:55:23 +0700

    Add project files.

---
+++ LAB1NEW/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using LAB1NEW.Repository;
+using LAB1NEW;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LAB1NEW
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private ServiceProvider serviceProvider;
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+            ConfigurationServices(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void ConfigurationServices(ServiceCollection services)
+        {
+            services.AddSingleton(typeof(ICarRepository), typeof(CarRepository));
+            services.AddSingleton<MainWindow>();
+        }
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            var windowCarManagement = serviceProvider.GetService<MainWindow>();
+            MainWindow.Show();
+        }
+    }
+
+}
---
+++ LAB1NEW/DataAccess/MyStockContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+
+namespace LAB1NEW.DataAccess
+{
+    public partial class MyStockContext : DbContext
+    {
+        public MyStockContext()
+        {
+        }
+
+        public MyStockContext(DbContextOptions<MyStockContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Car> Cars { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true)
+            .Build();
+            var strConn = config["ConnectionStrings:MyDatabase"];
+            optionsBuilder.UseSqlServer(strConn);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Car>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.CarId).HasColumnName("CarID");
+
+                entity.Property(e => e.CarName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Manufacturer)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Price).HasColumnType("money");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
